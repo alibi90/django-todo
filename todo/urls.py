f12011a01e5cedb304f164c9cb351902be7413a0
Hasnain Ali <hasnainali@localhost.localdomain>
2019-06-28 15:36:18 +0500

    create view template issue"

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
 # from .views import index
-from .views import IndexView
+from .views import IndexView, addTodo
 
 app_name = 'todo'
 urlpatterns = [
-	url(r'^$', IndexView.as_view(), name="index")
+    url(r'^$', IndexView.as_view(), name="index"),
+    url(r'^add$', addTodo, name="add"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,17 +1,39 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, redirect
+from django.views import generic, View
 
 from .models import TodoItem
 
 # def index(request):
-# 	all_todos = TodoItem.objects.all()
+# 	+all_todos = TodoItem.objects.all()
 # 	import pdb; pdb.set_trace()
 # 	return render(request, 'todo/index.html', {"todos": all_todos})
 
-class IndexView(generic.ListView):
-	# model = TodoItem
-	template_name = 'todo/index.html'
-	context_object_name = 'todos'
 
-	def get_queryset(self):
-		return TodoItem.objects.all()
+# class IndexView(generic.ListView):
+# 	# model = TodoItem
+# 	template_name = 'todo/index.html'
+# 	context_object_name = 'todos'
+
+# 	def get_queryset(self):
+# 		return TodoItem.objects.all()
+
+
+class IndexView(View):
+
+    def get(self, request):
+        all_todos = TodoItem.objects.all()
+        return render(request, 'todo/index.html', {"todos": all_todos})
+
+
+def addTodo(request):
+
+	import pdb; pdb.set_trace()
+
+	if request.method == "POST":
+		title = request.POST['title']
+		content = request.POST['content']
+		due_date = request.POST['due_date']
+
+		TodoItem.objects.create(title=title, content=content, due_date=due_date)
+	elif request.method == "GET":
+		return render(request, 'todo/add.html')
